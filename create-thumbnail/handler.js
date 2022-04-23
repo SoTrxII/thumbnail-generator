39ccfc360884a734b321df3938d8d360a59d5f76
SoTrx <-->
2022-04-23 13:34:21 +0200

    Adding function option parsing and matching tests

--- create-thumbnail/handler.js
+++ create-thumbnail/handler.js
@@ -4,6 +4,17 @@ const { container } = require("#core/inversify.config.js");
 const { TYPES } = require("#core/types.js");
 const { resolve } = require("path");
 const { readFile, unlink } = require("fs/promises");
+const {
+  InvalidPresetError,
+  OptimizationError,
+  ThumbnailSchemaError,
+} = require("../src/pkg/thumbnail-generator/thumbnail-generator");
+
+const DEFAULT = {
+  aspectRatio: 16 / 9,
+  size: { width: 1280, height: 720 },
+  optimizeImage: false,
+};
 
 /**
  * @openapi
@@ -22,18 +33,62 @@ module.exports = async (event, context) => {
   }
   const preset = event.path.replace(/\//g, "");
 
+  // Resolve options
+  // OptimizeImage : boolean -> default false
+  if (
+    event.query.optimizeImage !== undefined &&
+    event.query.optimizeImage !== true &&
+    event.query.optimizeImage !== false
+  ) {
+    return userError(
+      context,
+      new Error("Invalid option provided : optimizeImage must be a boolean")
+    );
+  }
+  const optimizeImage = event.query.optimizeImage ?? DEFAULT.optimizeImage;
+
+  // Size
+  // If neither width nor height is provided, default to 720p
+  // If only one of them is provided, keep the aspect ratio of 16/9
+  // An ok size is either undefined or a positive integer
+  const isSizeValid = (size) =>
+    size === undefined || (!isNaN(parseInt(size)) && Number(size) > 0);
+  if (!isSizeValid(event.query.width) || !isSizeValid(event.query.height)) {
+    return userError(
+      context,
+      new Error(
+        "Invalid option provided : width and height must be positive integer if specified"
+      )
+    );
+  }
+  // Define width is only height is specified, define height if only width is specified
+  if (event.query.width !== undefined && event.query.height === undefined) {
+    event.query.height = Math.ceil(event.query.width / DEFAULT.aspectRatio);
+  } else if (
+    event.query.width === undefined &&
+    event.query.height !== undefined
+  ) {
+    event.query.width = Math.ceil(event.query.height * DEFAULT.aspectRatio);
+  }
+  // Event at this point, a 1x1 size could make ffmpeg panic, but that's the user fault
+  const width = event.query.width ?? DEFAULT.size.width;
+  const height = event.query.height ?? DEFAULT.size.height;
+
+  // Resolve args, which can be either in body or in QS
   // Using a body in a GET request is kind of weird
   // But there is a serious chance the url might get longer than 2000 character
+  // Args validation is ensured by the preset itself
   const args = Object.assign(event.body, event.query);
+
   try {
     const path = await thg.buildWithPreset(preset, args, {
-      size: { width: 1280, height: 720 },
+      size: { width: width, height: height },
       fontDir: resolve(__dirname, "assets/fonts/"),
       defaultFontPath: "liberation-mono/LiberationMono-Regular.ttf",
-      optimizeImage: false,
+      optimizeImage: optimizeImage,
     });
     const fileBuffer = await readFile(path);
-    await unlink(path);
+    unlink(path).catch(console.log);
     return context
       .status(200)
       .headers({
@@ -41,7 +96,15 @@ module.exports = async (event, context) => {
       })
       .succeed(fileBuffer);
   } catch (e) {
-    return userError(context, e);
+    switch (e.constructor.name) {
+      // Instanceof is broken is JS when extending error type, so we're checking the class name
+      case InvalidPresetError.name:
+      case ThumbnailSchemaError.name:
+        return userError(context, e);
+      case OptimizationError.name:
+      default:
+        return context.status(500).fail(e);
+    }
   }
 };
 
--- create-thumbnail/handler.spec.js
+++ create-thumbnail/handler.spec.js
@@ -10,6 +10,100 @@ const {
 
 const { join } = require("path");
 
+describe("Create Thumbnail handler", () => {
+  // Copying all assets
+  beforeAll(() => {
+    copyRecursiveSync(join(__dirname, "../assets"), join(__dirname, "assets"));
+  });
+  it("Error on unknown preset", async () => {
+    const res = await handle(
+      {
+        path: "/unknown",
+        body: validThumb,
+        query: {},
+      },
+      new MockFunctionContext()
+    );
+    expect(res.statusCode).toBe(400);
+    expect(res.value.message).toContain("unknown");
+  });
+  it("Error on root call", async () => {
+    const res = await handle(
+      {
+        path: "/",
+        body: validThumb,
+      },
+      new MockFunctionContext()
+    );
+    expect(res.statusCode).toBe(400);
+    expect(res.value.message.toLowerCase()).toContain("no preset");
+  });
+  it("Default options value", async () => {
+    const res = await rpgThumbReq();
+    expect(res.statusCode).toBe(200);
+    expect(res.value).toBeInstanceOf(Buffer);
+    expect(res.value.length).toBeGreaterThan(0);
+  }, 20000);
+  describe("Invalid options", () => {
+    it("optimizeImage", async () => {
+      const res = await rpgThumbReq({
+        query: {
+          optimizeImage: 1,
+        },
+      });
+      expect(res.statusCode).toBe(400);
+      expect(res.value.message).toContain("optimizeImage");
+      const res2 = await rpgThumbReq({
+        query: {
+          optimizeImage: "ddd",
+        },
+      });
+      expect(res2.statusCode).toBe(400);
+      expect(res2.value.message).toContain("optimizeImage");
+    });
+    it("size :: 0", async () => {
+      const res = await rpgThumbReq({
+        query: {
+          width: 0,
+        },
+      });
+      expect(res.statusCode).toBe(400);
+      expect(res.value.message).toContain("width");
+    });
+    it("size :: negative", async () => {
+      const res = await rpgThumbReq({
+        query: {
+          width: -22323323,
+        },
+      });
+      expect(res.statusCode).toBe(400);
+      expect(res.value.message).toContain("width");
+    });
+    it("height", async () => {
+      const res = await rpgThumbReq({
+        query: {
+          width: "height",
+        },
+      });
+      expect(res.statusCode).toBe(400);
+      expect(res.value.message).toContain("height");
+    });
+  });
+  describe("Preset :: Thumb-RPG", () => {
+    it("Ok on normal conditions", async () => {
+      const res = await rpgThumbReq();
+      expect(res.statusCode).toBe(200);
+      expect(res.value).toBeInstanceOf(Buffer);
+      expect(res.value.length).toBeGreaterThan(0);
+    }, 200000);
+
+    // Deleting all assets
+    afterAll(() => {
+      rmSync(join(__dirname, "assets"), { recursive: true, force: true });
+    });
+  });
+});
+
 const validThumb = {
   gmsAvatarUrl: [
     "https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg",
@@ -26,26 +120,23 @@ const validThumb = {
   logoUrl:
     "https://upload.wikimedia.org/wikipedia/commons/9/9a/Gull_portrait_ca_usa.jpg",
 };
-describe("Create Thumbnail handler", () => {
-  // Copying all assets
-  beforeAll(() => {
-    copyRecursiveSync(join(__dirname, "../assets"), join(__dirname, "assets"));
-  });
-  it("dd", async () => {
-    const res = await handle(
-      {
-        path: "/thumb-rpg/",
-        body: validThumb,
-      },
-      new MockFunctionContext()
-    );
-    console.log(res.value);
-  }, 200000);
-  // Deleting all assets
-  afterAll(() => {
-    rmSync(join(__dirname, "assets"), { recursive: true, force: true });
-  });
-});
+
+/**
+ * Request a thumbnail with rpg preset
+ * @param opt
+ */
+async function rpgThumbReq(opt) {
+  const query = opt?.query ?? {};
+  const body = opt?.body ?? validThumb;
+  return await handle(
+    {
+      path: "/thumb-rpg/",
+      body: body,
+      query: query,
+    },
+    new MockFunctionContext()
+  );
+}
 
 function copyRecursiveSync(src, dest) {
   const exists = existsSync(src);
